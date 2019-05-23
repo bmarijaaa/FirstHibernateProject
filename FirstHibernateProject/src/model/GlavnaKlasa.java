f167a739982672654c60d690b568b3c3c8b51858
HP <HP@DESKTOP-J39MHQ0>
2019-05-23 20:03:41 +0200

    hql primer

--- FirstHibernateProject/src/model/GlavnaKlasa.java
+++ FirstHibernateProject/src/model/GlavnaKlasa.java
@@ -3,6 +3,8 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Query;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -16,19 +18,36 @@ public class GlavnaKlasa {
 	
 		
 		//CrudMetode metode = new CrudMetode();
-
+		//profCvijanovic
 			
 		SessionFactory sf = new Configuration().configure().buildSessionFactory();
 
 		User user = new User();
 		
+		String prezime = "Petrovic";
+		
+		List<User> listaUsera = null;
 		Session sesija = sf.openSession();
 		sesija.beginTransaction();
 			try {
 	
-				user = sesija.get(User.class, 84);
-					user.setPrezime("Petrovic");
-				sesija.update(user);
+				String hql = "FROM User WHERE idUser = 5";
+				
+				Query upit = sesija.createQuery(hql);
+				listaUsera = upit.getResultList();
+				
+				if(listaUsera.isEmpty()) {
+					System.out.println("Nema tih usera");
+				
+				}else{
+					for(User u:listaUsera ) {
+						
+						System.out.println(u.getIdUser() + " " + u.getIme() + " " + u.getPrezime());
+					}
+				}
+		
+				
+				
 				sesija.getTransaction().commit();
 			} catch (Exception e) {
 				sesija.getTransaction().rollback();
