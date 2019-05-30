9f0c63576bd9f58f52821c563196025f16a49b08
HP <HP@DESKTOP-J39MHQ0>
2019-05-30 18:54:16 +0200

    lazy init reseno

--- FirstHibernateProject/src/bussinesService/CrudMetode.java
+++ FirstHibernateProject/src/bussinesService/CrudMetode.java
@@ -3,6 +3,7 @@ package bussinesService;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -67,7 +68,30 @@ public class CrudMetode {
 		sesija.beginTransaction();
 			try {
 				user = sesija.get(User.class, idUser);
-			
+				
+				sesija.getTransaction().commit();
+				System.out.println("Uspesno preuzet user");
+				return user;
+			} catch (Exception e) {
+				sesija.getTransaction().rollback();
+				System.out.println("Neuspesno preuzet user");
+				return null;
+			}finally {
+				sesija.close();
+			}
+		
+	}
+	
+	public User vratiUseraSaListomMarki(int idUser) {
+		
+		User user = new User();
+		
+		Session sesija = sf.openSession();
+		sesija.beginTransaction();
+			try {
+				user = sesija.get(User.class, idUser);
+				//user.getListaMarki().size(); //preuzeo sam listu marki na 1. nacin
+				Hibernate.initialize(user.getListaMarki()); //preuzeo sam listu marki na 2. nacin
 				sesija.getTransaction().commit();
 				System.out.println("Uspesno preuzet user");
 				return user;
--- FirstHibernateProject/src/model/GlavnaKlasa.java
+++ FirstHibernateProject/src/model/GlavnaKlasa.java
@@ -12,22 +12,16 @@ public class GlavnaKlasa {
 			
 		CrudMetode metode = new CrudMetode();
 		
-		User user = metode.vratiUsera(2);
+		User user = metode.vratiUseraSaListomMarki(1);
 		
-		Marka m1 = metode.vratiMarku(1);
-		Marka m2 = metode.vratiMarku(3);
-		Marka m3 = metode.vratiMarku(4);
-		
-		
-		boolean spoji1 = metode.dodajMarkuUseru(m1, user);
-		boolean spoji2 = metode.dodajMarkuUseru(m2, user);
-		boolean spoji3 = metode.dodajMarkuUseru(m3, user);
-		
-		if(spoji1 && spoji2 && spoji3) {
-			System.out.println("Uspesno spojeni");
-		}else {
-			System.out.println("Greska u spoju!");
+		for(Marka m: user.getListaMarki()) {
+			System.out.println(m.getNazivMarke());
 		}
+		
+
+		
+		
+
 			
 			
 		
